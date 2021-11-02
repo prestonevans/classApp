54111a7badc71fa70640a65f643025080dd91ce8
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-11-01 19:32:05 -0600

    class fetch

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
-//document.body.style.backgroundColor = "red";
-//test me
+function getCourseInfo() {
+	const coursePromise = fetch('http://golf-courses-api.herokuapp.com/courses/11819').then((response) =>
+		response.json()
+	);
+	return coursePromise;
+}
+
+let coursePromise = getCourseInfo();
+
+coursePromise.then((data) => {
+	const course = data.data;
+
+	course.holes.forEach((hole) => {
+		document.body.innerHTML += `<div> 
+            <p>${hole.teeBoxes[0].par}</p>
+            <p>${hole.teeBoxes[0].yards}</p>
+            <p>${hole.teeBoxes[0].hcp}</p>
+        </div>`;
+	});
+});
